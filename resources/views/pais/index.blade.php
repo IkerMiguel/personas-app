48aed3824cb846877e1113a55a9b10650911cbe7
IkerMiguel <ikmiguel03@gmail.com>
2025-03-24 21:31:04 -0500

    Pais List

---
+++ resources/views/pais/index.blade.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Listado de paises</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container">
+        <h1>country list</h1>
+        <!--Boton para crear pais-->
+        <a href="" class="btn btn-success">Add</a>
+        <!--Boton para crear pais-->
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">Code</th>
+                    <th scope="col">country</th>
+                    <th scope="col">nationality</th>
+                    <th scope="col">Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($paises as $pais)
+                <tr>
+                    <th scope="row">{{ $pais->pais_codi}}</th>
+                    <td>{{ $pais->pais_nomb}}</td>
+                    <td>{{ $pais->pais_capi}}</td>
+                    <td>
+                        Actions
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+        <!-- Paginación -->
+        <div class="d-flex justify-content-center">
+            {{ $paises->links('pagination::bootstrap-5') }}
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+
+</html>
+
+
